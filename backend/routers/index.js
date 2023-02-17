58e4bbb4bdea6818d04c1b32e859a22662adf158
RizkyFadhilla <rizkyfadhilla123@gmail.com>
2023-02-17 14:59:08 +0700

    add routing

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const userRouter = require("./user")
+const productRouter = require("./product")
+const orderRouter = require("./order")
 
-
+router.use("/user",userRouter)
 router.get("/", async (req,res)=>{
     res.send("home")
 })
+router.use("/product",productRouter)
+router.use("/order",orderRouter)
 
 module.exports = router
